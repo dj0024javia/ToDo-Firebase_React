96e1f6b1bd5f188aa7b0b7441fc48d8646ae86e4
dj0024javia <31767853+dj0024javia@users.noreply.github.com>
2020-08-28 11:51:36 +0530

    final build

--- src/Todo.js
+++ src/Todo.js
@@ -1,25 +1,72 @@
-import React from 'react'
+import React, { useState } from 'react'
 import List from '@material-ui/core/List';
-import { ListItem, ListItemText, Button } from '@material-ui/core';
+import { ListItem, ListItemText, Button, Modal, makeStyles, Input } from '@material-ui/core';
 import "./Todo.css"
 import { db } from './firebase';
 
+const useStyles = makeStyles((theme) => ({
+    paper: {
+      position: 'absolute',
+      width: 400,
+      backgroundColor: theme.palette.background.paper,
+      border: '2px solid #000',
+      boxShadow: theme.shadows[5],
+      padding: theme.spacing(2, 4, 3),
+    },
+  }));
+
+
 function Todo(props) {
-    console.log(props)
+    
+    const [open, setOpen] = useState(false)
+    const [input, setInput] = useState('')
+
+    const classes = useStyles()
+
+    const handleOpen = () => {
+        setOpen(true);
+    }
+
+    const handleClose = () => {
+        setOpen(false);
+    }
+
+    const updateToDo =() => {
+        db.collection('collectiondj').doc(props.eachtodo.id).set(
+            {
+                ToDo: input,
+            }, {merge: true}
+        )
+        setOpen(false);
+    }
+
+
     return (
+        <>
+        <Modal className={classes.paper}
+            open={open}
+            onClose={handleClose}
+        >   
+            <div>
+                <Input placeholder={props.eachtodo.ToDo} value={input} onChange={event => setInput(event.target.value)} />
+                <Button onClick={e => updateToDo()}>Update</Button>
+            </div>
+        </Modal>
         <List>
-            <ListItem>
+            <ListItem> 
                 {console.log(props.eachtodo)}
                 <ListItemText primary={props.eachtodo.ToDo} secondary="Dummy Deadline!!" />
                 {/* Props is the property and text1 is the property of passed variable which is props. */}
                 {/* Props is the object now and eachtodo is the text. */}
             </ListItem>
+            <button onClick={e => setOpen(true)}>Edit</button>
             {/* {console.log(db.collection('collectiondj').doc(props))} */}
             <Button onClick={event => 
                 console.log(db.collection('collectiondj').doc(props.eachtodo.id).delete())
                 // db.collection('collectiondj').doc(props).delete
                 } >Delete</Button>
         </List>
+        </>
     )
 }
 
